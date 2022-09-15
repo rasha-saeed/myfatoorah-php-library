b7108ec5eeee6d8dc0ecb3e3b47bafc21563bc72
rs <rsaeed@myfatoorah.com>
2022-09-15 22:02:04 +0200

    test

--- src/PaymentMyfatoorahApiV2.php
+++ src/PaymentMyfatoorahApiV2.php
@@ -53,6 +53,193 @@ class PaymentMyfatoorahApiV2 extends MyfatoorahApiV2
         }
         return $paymentMethods;
     }
+    /**
+     *
+     * @param object $json
+     *
+     * @return object
+     */
+        protected function getSuccessData($json)
+    {
 
+        foreach ($json->Data->InvoiceTransactions as $transaction) {
+            if ($transaction->TransactionStatus == 'Succss') {
+                $json->Data->InvoiceStatus = 'Paid';
+                $json->Data->InvoiceError  = '';
 
+                $json->Data->focusTransaction = $transaction;
+                return $json->Data;
+            }
+        }
+        return $json->Data;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     *
+     * @param object $json
+     * @param string $keyId
+     * @param string $KeyType
+     *
+     * @return object
+     */
+    protected function getErrorData($json, $keyId, $KeyType)
+    {
+
+        //------------------
+        //case 1: payment is Failed
+        $focusTransaction = $this->{"getLastTransactionOf$KeyType"}($json, $keyId);
+        if ($focusTransaction && $focusTransaction->TransactionStatus == 'Failed') {
+            $json->Data->InvoiceStatus = 'Failed';
+            $json->Data->InvoiceError  = $focusTransaction->Error . '.';
+
+            $json->Data->focusTransaction = $focusTransaction;
+
+            return $json->Data;
+        }
+
+        //------------------
+        //case 2: payment is Expired
+        //all myfatoorah gateway is set to Asia/Kuwait
+        $ExpiryDateTime = $json->Data->ExpiryDate . ' ' . $json->Data->ExpiryTime;
+        $ExpiryDate     = new \DateTime($ExpiryDateTime, new \DateTimeZone('Asia/Kuwait'));
+        $currentDate    = new \DateTime('now', new \DateTimeZone('Asia/Kuwait'));
+
+        if ($ExpiryDate < $currentDate) {
+            $json->Data->InvoiceStatus = 'Expired';
+            $json->Data->InvoiceError  = 'Invoice is expired since ' . $json->Data->ExpiryDate . '.';
+
+            return $json->Data;
+        }
+
+        //------------------
+        //case 3: payment is Pending
+        //payment is pending .. user has not paid yet and the invoice is not expired
+        $json->Data->InvoiceStatus = 'Pending';
+        $json->Data->InvoiceError  = 'Pending Payment.';
+
+        return $json->Data;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     *
+     * @param object         $json
+     * @param integer|string $keyId
+     *
+     * @return object
+     */
+    protected function getLastTransactionOfPaymentId($json, $keyId)
+    {
+
+        foreach ($json->Data->InvoiceTransactions as $transaction) {
+            if ($transaction->PaymentId == $keyId && $transaction->Error) {
+                return $transaction;
+            }
+        }
+    }
+
+    /**
+     *
+     * @param object $json
+     *
+     * @return object
+     */
+    protected function getLastTransactionOfInvoiceId($json)
+    {
+
+        usort($json->Data->InvoiceTransactions, function ($a, $b) {
+            return strtotime($a->TransactionDate) - strtotime($b->TransactionDate);
+        });
+
+        return end($json->Data->InvoiceTransactions);
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Refund a given Payment (POST API)
+     *
+     * @param integer|string        $paymentId    payment id that will be refunded
+     * @param double|integer|string $amount       the refund amount
+     * @param string                $currencyCode the refund currency
+     * @param string                $reason       reason of the refund
+     * @param integer|string        $orderId      used in log file (default value: null)
+     *
+     * @return object
+     */
+    public function refund($paymentId, $amount, $currencyCode, $reason, $orderId = null)
+    {
+
+        $rate = $this->getCurrencyRate($currencyCode);
+        $url  = "$this->apiURL/v2/MakeRefund";
+
+        $postFields = [
+            'KeyType'                 => 'PaymentId',
+            'Key'                     => $paymentId,
+            'RefundChargeOnCustomer'  => false,
+            'ServiceChargeOnCustomer' => false,
+            'Amount'                  => $amount / $rate,
+            'Comment'                 => $reason,
+        ];
+
+        return $this->callAPI($url, $postFields, $orderId, 'Make Refund');
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Create an invoice using Embedded session (POST API)
+     *
+     * @param array          $curlData  invoice information
+     * @param integer|string $sessionId session id used in payment process
+     * @param integer|string $orderId   used in log file (default value: null)
+     *
+     * @return array
+     */
+    public function embeddedPayment($curlData, $sessionId, $orderId = null)
+    {
+
+        $curlData['SessionId'] = $sessionId;
+
+        $json = $this->callAPI("$this->apiURL/v2/ExecutePayment", $curlData, $orderId, 'Embedded Payment');
+        return ['invoiceURL' => $json->Data->PaymentURL, 'invoiceId' => $json->Data->InvoiceId];
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Get session Data (POST API)
+     *
+     * @param string         $userDefinedField Customer Identifier to dispaly its saved data
+     * @param integer|string $orderId          used in log file (default value: null)
+     *
+     * @return object
+     */
+    public function getEmbeddedSession($userDefinedField = '', $orderId = null)
+    {
+
+        $customerIdentifier = ['CustomerIdentifier' => $userDefinedField];
+        return $this->callAPI("$this->apiURL/v2/InitiateSession", $customerIdentifier, $orderId, 'Initiate Session');
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------
+
+    /**
+     * Register Apple Pay Domain (POST API)
+     *
+     * @param string $url Site URL
+     *
+     * @return object
+     */
+    public function registerApplePayDomain($url)
+    {
+
+        $domainName = ['DomainName' => parse_url($url, PHP_URL_HOST)];
+        return $this->callAPI("$this->apiURL/v2/RegisterApplePayDomain", $domainName, '', 'Register Apple Pay Domain');
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------------------
 }
